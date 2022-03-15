37b44a91ab8dd4c7867a101e03c414ec85506ba9
mitochon9 <mitochon9@gmail.com>
2022-03-15 18:50:55 +0900

    style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
+import { useSetRecoilState } from 'recoil';
 import { Form } from '@/component/molecule/Form';
 import { Footer } from '@/component/organism/Footer';
 import { Header } from '@/component/organism/Header';
+import { valueState } from '@/state/valueState';
 
 type FormData = {
   value: string;
 };
 
 const Home: React.FC = () => {
+  const router = useRouter();
+  const setValue = useSetRecoilState(valueState);
+
   const {
     register,
     formState: { errors },
@@ -16,25 +22,29 @@ const Home: React.FC = () => {
 
   const onSubmit = (data: FormData) => {
     console.log(data);
+    setValue(data.value);
+    router.push('/orLater');
   };
 
   return (
     <>
-      <Header />
-      <Form
-        labelId='worries'
-        labelName='value'
-        inputPlaceholder='入力'
-        inputRegister={register}
-        inputErrors={errors.value}
-        buttonLabel={'次に進む'}
-        buttonType='submit'
-        buttonVariant='primary'
-        title='深めたいことや悩みを入力'
-        orLater={false}
-        onSubmit={handleSubmit(onSubmit)}
-      />
-      <Footer />
+      <div className='flex h-screen flex-col items-center justify-between text-center'>
+        <Header />
+        <Form
+          labelId='worries'
+          labelName='value'
+          inputPlaceholder='入力'
+          inputRegister={register}
+          inputErrors={errors.value}
+          buttonLabel={'次に進む'}
+          buttonType='submit'
+          buttonVariant='primary'
+          title='深めたいことや悩みを入力'
+          orLater={false}
+          onSubmit={handleSubmit(onSubmit)}
+        />
+        <Footer />
+      </div>
     </>
   );
 };
