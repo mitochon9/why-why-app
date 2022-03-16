4ee998b1322826d516327726a4704cd251df242e
mitochon9 <mitochon9@gmail.com>
2022-03-16 14:35:52 +0900

    gtm

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { googleTagManagerId } from 'src/utils/gtm';
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+    };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html prefix='og: https://ogp.me/ns#'>
+        <Head />
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              />`,
+            }}
+          />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
